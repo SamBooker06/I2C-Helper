a554fe6b263553f77416a35eac6603cd9b1de16a
Sam Booker <sam.booker@skships.co.uk>
2026-08-10 14:20:09 +0100

    Added support for SigmaStudio XMLs

---
+++ src/i2c_helper/sigmastudio/command.py
@@ -0,0 +1,60 @@
+from abc import ABC, ABCMeta, abstractmethod
+from time import sleep
+from typing import Optional
+
+from i2c_helper import I2CDriver
+
+
+class SequenceCommand(ABC, metaclass=ABCMeta):
+    @abstractmethod
+    def execute(self) -> Optional[bytes]:
+        raise NotImplementedError()
+
+
+class I2CCommand(SequenceCommand, metaclass=ABCMeta):
+    def __init__(self, driver: I2CDriver, device_address: int, memory_address: int, memory_address_size: int, ):
+        self.driver = driver
+
+        self.device_address = device_address
+        self.memory_address = memory_address
+
+        self.memory_address_size = memory_address_size
+
+
+class I2CReadCommand(I2CCommand):
+    def __init__(self, driver: I2CDriver, device_address: int, memory_address: int, memory_address_size: int,
+                 buffer_size: int) -> None:
+        super().__init__(driver, device_address, memory_address, memory_address_size)
+
+        self.buffer_size = buffer_size
+
+    def execute(self) -> bytes:
+        return self.driver.read(self.device_address, self.memory_address, buffer_size=self.buffer_size,
+                                memory_address_size=self.memory_address_size)
+
+
+class I2CWriteCommand(I2CCommand):
+
+    def __init__(self, driver: I2CDriver, device_address: int, memory_address: int, memory_address_size: int,
+                 data: bytes):
+        super().__init__(driver, device_address, memory_address, memory_address_size)
+
+        self.data = data
+
+    def execute(self) -> None:
+        self.driver.write(self.device_address, self.memory_address, self.data,
+                          memory_address_size=self.memory_address_size)
+
+
+class NoOpCommand(SequenceCommand):
+    def execute(self) -> None:
+        # Do nothing
+        return
+
+
+class DelayCommand(SequenceCommand):
+    def __init__(self, delay_ms: int) -> None:
+        self.delay_ms = delay_ms
+
+    def execute(self) -> None:
+        sleep(self.delay_ms / 1_000)
---
+++ src/i2c_helper/sigmastudio/sequence.py
@@ -0,0 +1,102 @@
+from enum import Enum
+
+from i2c_helper import I2CDriver
+from i2c_helper.sigmastudio.command import SequenceCommand, NoOpCommand, DelayCommand, I2CWriteCommand
+
+
+class SequenceInstruction(Enum):
+    Write = "writeXbytes"
+    Read = "read"
+    Delay = "delay"
+    NoOp = "noop"
+
+
+class Sequence:
+    @staticmethod
+    def from_xml(
+            xml: str,
+            driver: I2CDriver, replace_reads: bool = True
+    ) -> list["Sequence"]:
+        from xml.etree import ElementTree
+
+        root = ElementTree.fromstring(xml)
+
+        sequences = []
+
+        for page in root.findall("page"):
+            commands = []
+
+            mode_type = page.get("mode_type", None)
+            assert mode_type is not None, "mode_type field missing"
+
+            for action in page.findall("action"):
+                instruction = action.get("instr")
+                assert instruction is not None, "instruction field not found"
+
+                instruction = SequenceInstruction(instruction)
+
+                if instruction == SequenceInstruction.Write:
+                    i2caddr = action.get("i2caddr")
+                    addr = action.get("addr")
+                    addr_width = action.get("addr_width")
+
+                    assert i2caddr is not None, "i2caddr not found"
+                    assert addr is not None, "addr not found"
+                    assert addr_width is not None, "addr_width not found"
+
+                    device_address = int(i2caddr, 10)
+                    memory_address = int(addr, 10)
+                    memory_address_size = int(addr_width, 10)
+
+                    data = bytes.fromhex(action.text or "")
+
+                    commands.append(
+                        I2CWriteCommand(
+                            driver=driver,
+                            device_address=device_address,
+                            memory_address=memory_address,
+                            memory_address_size=memory_address_size,
+                            data=data,
+                        )
+                    )
+
+                elif instruction == SequenceInstruction.Delay:
+                    delay_ms = int.from_bytes(
+                        bytes.fromhex(action.text or ""),
+                        byteorder="big",
+                    )
+
+                    commands.append(
+                        DelayCommand(delay_ms=delay_ms)
+                    )
+
+                elif instruction == SequenceInstruction.NoOp:
+                    commands.append(NoOpCommand())
+
+                elif instruction == "read":
+                    if replace_reads:
+                        commands.append(NoOpCommand())
+
+                    else:
+                        raise ValueError("Read command present, and not told to skip")
+
+                else:
+                    raise ValueError(
+                        f"Unsupported SigmaStudio instruction: {instruction!r}"
+                    )
+
+            sequences.append(Sequence(*commands, mode_type=mode_type))
+
+        return sequences
+
+    def __init__(self, *commands: SequenceCommand, mode_type: str):
+        self.commands = commands
+        self.mode_type = mode_type
+
+    @property
+    def mode(self):
+        return self.mode_type
+
+    def execute(self) -> None:
+        for command in self.commands:
+            command.execute()
